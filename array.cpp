d80d3e64247b05f52d045015b21508260e835555
Yousef Elbilkasy <yousef2004bilkasy@gmail.com>
2024-04-15 03:14:36 +0200

    add reverse function

--- array.cpp
+++ array.cpp
@@ -17,6 +17,19 @@ public:
     ptr = new int[size];
   }
 
+  void reverse()
+  {
+    if (length > 0)
+    {
+      int temp, lastIndex = length - 1;        //  0  1  2  3
+      for (int i = 0; i < length / 2; i++)     // 10 20 30 40
+      {
+        temp = ptr[i];                  // temp = 10
+        ptr[i] = ptr[lastIndex]; // ptr[0] = 40
+        ptr[lastIndex--] = temp;   // ptr[3] = 10
+      }
+    }
+  }
   void fill()
   {
     int numberOfItems;
@@ -53,7 +66,22 @@ public:
   {
     return size;
   }
-  int search(int key)
+  int searchBinary(int key)
+  {
+    int left = 0;
+    int right = getLength() - 1;
+    while (left <= right)
+    {
+      int mid = (left + right) / 2;
+      if (ptr[mid] == key)
+        return mid;
+      else if (ptr[mid] > ptr[left])
+        left = mid + 1;
+      else
+        right = mid - 1;
+    }
+  }
+  int searchSequential(int key)
   {
     for (int i = 0; i < length; i++)
       if (ptr[i] == key)
@@ -148,7 +176,6 @@ public:
 
 int main()
 {
-  cout << "Demo" << endl;
   int size;
   cout << "Please, Enter The Size of Array" << endl;
   cin >> size;
@@ -158,7 +185,7 @@ int main()
   // cout << "Enter The Value You Want Index Of it For searching" << endl;
   // int key;
   // cin >> key;
-  // int index = myArray.search(key);
+  // int index = myArray.searchSequential(key);
   // cout << index << endl;
   // cout << "Enter Number You Want To Insert It: ";
   // int newNum;
@@ -179,10 +206,13 @@ int main()
   // myArray.enLarge(newSize);
   // cout << "Array Size: " << myArray.getSize() << "  Array Length: " << myArray.getLength() << endl;
   // myArray.display();
-  cout << "Other Array\n";
-  Array other(3);
-  other.fill();
-  myArray.merge(other);
-  cout << "Array Size: " << myArray.getSize() << "  Array Length: " << myArray.getLength() << endl;
+  // cout << "Other Array\n";
+  // Array other(3);
+  // other.fill();
+  // myArray.merge(other);
+  // cout << "Array Size: " << myArray.getSize() << "  Array Length: " << myArray.getLength() << endl;
+  // myArray.display();
+  cout << "Array Reversed: \n";
+  myArray.reverse();
   myArray.display();
 }
